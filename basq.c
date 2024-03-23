53e1f9e60b310c12b7e3e0aba8f88c99afd51351
gentle <gentle@G3ntle.G3ntle.com>
2024-03-23 03:31:20 -0300

    atualizacao

--- basq.c
+++ basq.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
-int main(){
-    
+void menu(){
+    printf("\n");
+    printf("Convertedor de Estatisticas de basquete em porcentagem\n");
+    printf("______________________________________________________________\n");
+    printf("Porcentagem de acertos de cestas arremessadas\n");
+    printf("porcentagem de acertos de cestas de 3\n");
+}
+
+void convesta(){
+    int cestas, ceserro, porc;
+
+    printf("insira quantas acertos o jogador acertou\n");
+    scanf("%d",cestas);
+
+    printf("insira quantos cestas o jogador errou\n");
+    scanf("%d",ceserro);
+
+    porc = (cestas + ceserro)/100
 }
